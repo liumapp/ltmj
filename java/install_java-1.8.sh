c8e9a4cbcdbe898697d0f87b0f9ea1cdeb7b2161
liumapp <liumapp.com@gmail.com>
2017-08-04 16:05:35 +0800

    删去多余的文件

--- java/install_java-1.8.sh
+++ java/install_java-1.8.sh
@@ -8,6 +8,23 @@
 #Time: 下午2:48
 #
 
-rm -rf jre1.8.0_131
-tar zxvf jre-8u131-linux-i586.tar.gz
-mv jre1.8.0_131 /lmdata/server/java-1.8/jre
+if [ `uname -m` == "x86_64" ];then
+machine=x86_64
+else
+machine=i686
+fi
+if [ $machine == "x86_64" ];then
+  rm -rf jdk1.8.0_131
+  if [ ! -f mysql-5.5.37-linux2.6-x86_64.tar.gz ];then
+	 wget http://test-oracle.oss-cn-hangzhou.aliyuncs.com/mysql-5.5.37-linux2.6-x86_64.tar.gz
+  fi
+  tar -xzvf mysql-5.5.37-linux2.6-x86_64.tar.gz
+  mv mysql-5.5.37-linux2.6-x86_64/* /alidata/server/mysql
+else
+  rm -rf mysql-5.5.37-linux2.6-i686
+  if [ ! -f mysql-5.5.37-linux2.6-i686.tar.gz ];then
+    wget http://test-oracle.oss-cn-hangzhou.aliyuncs.com/mysql-5.5.37-linux2.6-i686.tar.gz
+  fi
+  tar -xzvf mysql-5.5.37-linux2.6-i686.tar.gz
+  mv mysql-5.5.37-linux2.6-i686/* /alidata/server/mysql
+fi
