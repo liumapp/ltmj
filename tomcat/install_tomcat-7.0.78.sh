fa72731549b5e897aeb08046c417c31c5200228d
liumapp <liumapp.com@gmail.com>
2017-08-07 08:54:04 +0800

    tomcat source file and install shell

--- tomcat/install_tomcat-7.0.78.sh
+++ tomcat/install_tomcat-7.0.78.sh
@@ -4,6 +4,17 @@
 #User: liumapp
 #Email: liumapp.com@gmail.com
 #homePage: http://www.liumapp.com
-#Date: 8/4/17
-#Time: 7:20 PM
+#Date: 8/5/17
+#Time: 4:43 PM
 #
+
+## install tomcat  -- begin ##
+  rm -rf apache-tomcat-7.0.78
+  if [ ! -f apache-tomcat-7.0.78.tar.gz ];then
+	 echo "ERROR ! Tomcat source file cannot be found "
+	 exit 0
+  fi
+  tar -xzvf apache-tomcat-7.0.78.tar.gz
+  mv apache-tomcat-7.0.78/* /alidata/server/tomcat
+## install tomcat -- end ##
+
